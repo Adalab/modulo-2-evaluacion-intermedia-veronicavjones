40ec78fadda182b7236d75fd357f488f8925391a
veronicavjones <veronicavjones@hotmail.com>
2023-02-14 11:01:12 +0100

    adding js functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,37 @@
 'use strict';
 
+//variables elementos html
 const button = document.querySelector('.js_btn');
 const pista = document.querySelector('.js_pista');
 const intentos = document.querySelector('.js_intentos');
+
+//funciones
+function tooHigh() {
+
+}
+
+function tooLow() {
+
+}
+
+function youWin() {
+
+}
+
+function numberBetween() {
+    
+}
+//eventos
+function handleClickButton(event) {
+    event.preventDefault();
+    console.log('funciona');
+
+    tooHigh();
+    tooLow();
+    youWin();
+    numberBetween();
+
+}
+
+
+button.addEventListener('click', handleClickButton);

65fb3de7d6ec55f043b825a6e91f3fb41b7e072c
veronicavjones <veronicavjones@hotmail.com>
2023-02-16 19:48:53 +0100

    comparing random number

--- js/main.js
+++ js/main.js
@@ -1,36 +1,36 @@
 'use strict';
 
 //variables elementos html
+const userNumber =document.querySelector('.js_userNumber');
 const button = document.querySelector('.js_btn');
 const pista = document.querySelector('.js_pista');
 const intentos = document.querySelector('.js_intentos');
 
 //funciones
-function tooHigh() {
+function getRandomNumber(max) {
+ return Math.ceil(Math.random() * max);
+}
+const randomNumber = getRandomNumber(100);
+
+console.log(randomNumber)
+
+function compareNumber(){
+    const valueUserNumber = userNumber.value;
+    if(valueUserNumber < 1 || valueUserNumber >100){
+       pista.innerHTML = 'el número debe estar entre 1 y 100';
+    }
 
 }
 
-function tooLow() {
 
-}
 
-function youWin() {
 
-}
 
-function numberBetween() {
-    
-}
 //eventos
 function handleClickButton(event) {
     event.preventDefault();
     console.log('funciona');
-
-    tooHigh();
-    tooLow();
-    youWin();
-    numberBetween();
-
+    compareNumber();
 }
 
 
